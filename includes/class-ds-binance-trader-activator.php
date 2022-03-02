7451e3509270bc8ab1951c84c9c77d3d82c6507a
Esubalew Amenu <esubalew.a2009@gmail.com>
2022-03-03 02:01:49 +0300

    status columns added

--- includes/class-ds-binance-trader-activator.php
+++ includes/class-ds-binance-trader-activator.php
@@ -58,6 +58,9 @@ class Ds_Binance_Trader_Activator
 			$sql .= "  `price` varchar(50) NOT NULL, ";
 
 			$sql .= "  `status` varchar(10) NOT NULL, "; //NEW,
+			$sql .= "  `buy_id` int(10) unsigned, ";
+			$sql .= "  `profit_loss` varchar(10), "; //p, l
+			$sql .= "  `profit_loss_amount` varchar(10), ";
 			
 			$sql .= "  `orderId` varchar(255) NOT NULL, ";
 			$sql .= "  `orderListId` varchar(255) NOT NULL, ";
